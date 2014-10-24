6e15922190b6dcf0a81bb23a9f76a2863b57748e
40in <40in@yandex-team.ru>
2014-10-24 17:45:48 +0400

    server templating

--- roman/myjs/app.js
+++ roman/myjs/app.js
@@ -6,8 +6,8 @@
       var Workspace = Backbone.Router.extend({
 
         routes: {
-          "#": "homeAction",
-          "#about": "aboutAction"
+          "": "homeAction",
+          "about": "aboutAction"
         },
 
         homeAction: function() {
